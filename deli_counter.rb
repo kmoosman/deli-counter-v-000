fa0643f13477506ec1ea77a413479c59539ba509
moosman.katie <moosman.katie@gmail.com>
2018-12-04 23:23:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def line(katz_deli)
+  puts "The line is currently empty."
+end 
+
+def take_a_number(katz_deli, name)
+  puts "#{name} you are #{katz_deli.length + 1} in line"
+end 
