1b00c4e03b7e2e1a43d4577186e8df23ca5be159
moosman.katie <moosman.katie@gmail.com>
2018-12-05 00:05:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.count == 0 
+    puts "The line is currently empty."
+  else 
+    katz_deli.each_with_index {|name, i| line.push("#{i +1 }.", name)}
+    puts "The line is currently: #{line.join(" ")}"
+  end 
+end 
+
+def take_a_number(katz_deli, name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
+end 
